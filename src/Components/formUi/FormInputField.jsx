d79b666b276cce0984a03428bac9aa79945a4eee
SaharMahmoudDev <elzerossss@gmail.com>
2026-04-17 21:25:22 +0200

    update

--- src/Components/formUi/FormInputField.jsx
+++ src/Components/formUi/FormInputField.jsx
@@ -1,26 +1,17 @@
-import React from "react";
-import { Field, FieldLabel, FieldError } from "@/Components/ui/field";
-import { Input } from "@/Components";
+import React from 'react'
+import { Field ,FieldLabel,FieldError} from "@/Components/ui/field"
+import {Input} from '@/Components'
 
-const FormInputField = ({
-  form,
-  type,
-  name,
-  placeholder,
-  label,
-  classField,
-}) => {
+const FormInputField = ({form,type,name,placeholder,label,classField}) => {
   return (
     <form.Field
       name={name}
       children={(field) => {
         const isInvalid =
-          field.state.meta.isTouched && !field.state.meta.isValid;
+          field.state.meta.isTouched && !field.state.meta.isValid
         return (
-          <Field className={classField} data-invalid={isInvalid}>
-            <FieldLabel className="sr-only" htmlFor={field.name}>
-              {label}
-            </FieldLabel>
+          <Field className={classField}data-invalid={isInvalid}>
+            <FieldLabel className='sr-only' htmlFor={field.name}>{label}</FieldLabel>
             <Input
               id={field.name}
               name={field.name}
@@ -31,18 +22,18 @@ const FormInputField = ({
               aria-invalid={isInvalid}
               placeholder={placeholder}
               autoComplete="off"
-              className="max-w-[421px] mx-auto pl-0 border-0 border-b
+               className="max-w-[421px] mx-auto pl-0 border-0 border-b
                 border-[rgba(0,_0,_0,_0.12)] bg-transparent rounded-none 
                 focus-visible:ring-0 transition-colors placeholder:text-lg
                  placeholder:text-[rgba(0,0,0,0.32)] placeholder:font-normal placeholder:capitalize"
             />
-
+            
             {isInvalid && <FieldError errors={field.state.meta.errors} />}
           </Field>
-        );
+        )
       }}
     />
-  );
-};
+  )
+}
 
-export default FormInputField;
+export default FormInputField
